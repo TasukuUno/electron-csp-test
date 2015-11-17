8d7d116e7aea758d48d5a6892fd2066e5519e15b
uno_tasuku <uno_tasuku@cyberagent.co.jp>
2015-11-18 03:02:10 +0900

    add sample app

--- common/index.js
+++ common/index.js
@@ -6,4 +6,32 @@
 
   $('#location').textContent = location.href;
 
+  $('#play').addEventListener('click', function () {
+    get('audio-test-001://', function (buffer) {
+      if (buffer) {
+        var audio = new Audio();
+        var blob = new Blob([buffer], {type: 'audio/mpeg'});
+        audio.src = URL.createObjectURL(blob);
+        audio.play();
+      }
+    });
+  }, false);
+
+  function get(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'arraybuffer';
+    xhr.onreadystatechange = function () {
+      var state = xhr.readyState;
+      if (state === 4) {
+        if (xhr.status === 200) {
+          var buffer = new Uint8Array(xhr.response);
+          callback(buffer, xhr);
+        } else {
+          callback();
+        }
+      }
+    };
+    xhr.send(null);
+  }
 })();
---
+++ electron/main/index.js
@@ -0,0 +1,34 @@
+var electron = require('electron');
+var app = electron.app;
+var BrowserWindow = electron.BrowserWindow;
+
+var fs = require('fs');
+var path = require('path');
+
+app.on('window-all-closed', function() {
+  app.quit();
+});
+
+app.on('ready', function() {
+  registerProtocol();
+  openWindow();
+});
+
+function registerProtocol() {
+  var protocol = electron.protocol;
+  protocol.registerBufferProtocol('audio-test-001', function (request, callback) {
+    console.log('[protocol request]', request);
+    var filePath = path.join(__dirname, 'sample.mp3');
+    fs.readFile(filePath, function (err, data) {
+      if (err) return console.log('can not get audio', filePath, err);
+      callback(data);
+    });
+  }, function (error) {
+    if (error) console.log(error);
+  });
+}
+
+function openWindow() {
+  var mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadURL('file://' + __dirname + '/../renderer/index.html');
+}
